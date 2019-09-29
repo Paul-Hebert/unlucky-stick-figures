b59d55fcebfe37d5de7cf279b5d330ceb21a05db
Paul Hebert <paul@cloudfour.com>
2019-09-29 10:21:04 -0700

    Configure layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
-module.exports = function() {
+const path = require('path');
+const fg = require('fast-glob');
+
+const layoutsDir = 'src/_includes/layouts/';
+
+module.exports = eleventyConfig => {
+  fg.sync(layoutsDir + '*.hbs').forEach(file => {
+    const basename = path.basename(file, '.hbs');
+    eleventyConfig.addLayoutAlias(basename, `layouts/${basename}.hbs`);
+  });
+
   return {
     dir: {
       input: 'src',
